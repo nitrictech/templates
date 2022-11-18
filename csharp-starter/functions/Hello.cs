1c2af105ead8ba2cf2799a3910139beaa6ecc9c3
Ryan Cartwright <fryzee.cartwright@gmail.com>
2022-11-18 11:56:38 +1100

    feat: add csharp template

---
+++ csharp-starter/functions/Hello.cs
@@ -0,0 +1,19 @@
+﻿using Nitric.Sdk;
+using System;
+
+var serviceAddress = System.Environment.GetEnvironmentVariable("SERVICE_ADDRESS");
+
+Console.WriteLine(serviceAddress);
+
+var api = Nitric.Sdk.Nitric.Api("main");
+
+api.Get("/hello/:name", context =>
+{
+    var name = context.Req.PathParams["name"];
+
+    context.Res.Text($"Hello {name}!");
+
+    return context;
+});
+
+Nitric.Sdk.Nitric.Run();
