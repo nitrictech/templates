b0b1c13a293152b98af2cf2b1c73d55f99418850
Ryan Cartwright <fryzee.cartwright@gmail.com>
2023-05-04 17:51:35 +1000

    update python sdk version and fix windows pipenv run issue

--- python-starter/functions/hello.py
+++ python-starter/functions/hello.py
@@ -1,11 +1,12 @@
 from nitric.resources import api
 from nitric.application import Nitric
+from nitric.faas import HttpContext
 
 helloApi = api("main")
 
 
 @helloApi.get("/hello/:name")
-async def hello_world(ctx):
+async def hello_world(ctx: HttpContext):
     name = ctx.req.params['name']
 
     ctx.res.body = f"Hello {name}"
