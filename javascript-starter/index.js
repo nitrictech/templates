7a8574341848539ac3acc1a1a9e9d8bb14485b41
David Moore <davemooreuws@gmail.com>
2023-05-08 12:35:53 +1000

    read nitric yaml to get glob handlers for hot reload

---
+++ javascript-starter/index.js
@@ -0,0 +1,23 @@
+// index.js Used in development for hot reloading/nodemon
+import fs from "fs";
+import yaml from "js-yaml";
+import { globSync } from "glob";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config = yaml.load(fs.readFileSync("nitric.yaml", "utf8"));
+
+if (!config.handlers) {
+  throw new Error("Handlers not defined in config file");
+}
+
+config.handlers.forEach((handlerGlob) => {
+  const files = globSync(`${__dirname}/${handlerGlob}`);
+  files
+    .filter((file) => file.slice(-3) === ".js")
+    .forEach((file) => {
+      import(`./${file}`);
+    });
+});
